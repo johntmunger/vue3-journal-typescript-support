cf2bc6d52734dc7356576099d8fb6cf8112b3e0d
johntmunger <johntmunger@gmail.com>
2025-11-14 12:00:32 -0700

    defineProps to the entry card comnponent from the editor component

--- src/composables/UseEmojis.ts
+++ src/composables/UseEmojis.ts
@@ -1,25 +1,22 @@
+import Emoji from "@/types/Emoji";
 import type { Component } from "vue";
 import { reactive } from "vue";
 
-const files: { [key: string]: unknown } = import.meta.glob(
-  "../assets/icons/emojis/*.svg",
-  { eager: true }
-);
+const files: { [key: string]: unknown } = import.meta.glob("../assets/icons/emojis/*.svg", {
+  eager: true,
+});
 
 export default function UseEmojis() {
   const emojis: { name: string; component: Component }[] = reactive([]);
   for (const path in files) {
     const component = files[path] as Component;
-    const name = path
-      .replace("../assets/icons/emojis/", "")
-      .replace("-emoji.svg", "");
+    const name = path.replace("../assets/icons/emojis/", "").replace("-emoji.svg", "");
     emojis.push({ name, component });
   }
   /**
    * Get a single emoji component by name
    */
-  const findEmoji = (name: string) =>
-    emojis.find((emoji) => emoji.name === name)?.component;
+  const findEmoji = (name: Emoji | null) => emojis.find((emoji) => emoji.name === name)?.component;
 
   return { emojis, findEmoji };
 }
